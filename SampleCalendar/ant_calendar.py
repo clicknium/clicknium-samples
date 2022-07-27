f701b25fc6d6d2526e983bb56efa45c95b587e50
xiaoxinbo118 <xiaoxinbo118@163.com>
2022-07-27 16:14:35 +0800

    canlendar demo

--- SampleCalendar/ant_calendar.py
+++ SampleCalendar/ant_calendar.py
@@ -1,20 +1,20 @@
 from clicknium import clicknium, ui, locator
 
-# 输入项
+# Input
 year = '2010'
 mouth = 'Jan'
 day = '1'
 
-# 对应日期控件中月份常量
+# The month corresponding to the date control
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-# 打开页面
+# Open the page
 clicknium.chrome.open("https://ant.design/components/date-picker/")
 
-# 点击日期输入框
+# Click the date input
 ui(locator.chrome.ant.text).mouse_down()
 
-# 比较年份是否一致，不一致的情况通过上一年，下一年按钮调整，直到一致
+# Compare the year. If not same, adjust to the next year or last year by clicking the button until they are same.
 locator_year = ui(locator.chrome.ant.button_year)
 current_year = locator_year.get_property(name="sInfo")
 
@@ -25,7 +25,7 @@ while year != current_year:
         ui(locator.chrome.ant.button_year_pre).click()
     current_year = locator_year.get_property(name="sInfo")
 
-# 比较月份是否一致，不一致的情况通过上一月，下一月按钮调整，直到一致
+# Compare the month. If not same, adjust to the next month or last month by clicking the button until they are same.
 locator_mouth = ui(locator.chrome.ant.button_mouth)
 current_mouth = locator_mouth.get_property(name="sInfo")
 current_mouth_index = months.index(current_mouth)
@@ -39,5 +39,5 @@ while current_mouth_index != mouth_index:
     current_mouth = locator_mouth.get_property(name="sInfo")
     current_mouth_index = months.index(current_mouth)
 
-# 用日期变量定位到对应的日期元素，并点击
+# locate the date element with the date variable and click it
 ui(locator.chrome.ant.div_day, {"day": day }).click()
